857742bc97e2a2e9538a0f1901c0d5d66100ecb5
QMaximillian <quinnlashinsky@gmail.com>
2018-10-14 21:48:24 -0400

    Added service_url as rendered json to frontend

--- app/models/tryout.rb
+++ app/models/tryout.rb
@@ -5,6 +5,6 @@ class Tryout < ApplicationRecord
   has_one_attached :resume
 
   def format
-    {tryout: self, audition_journal: self.audition_journal, resume: self.resume.download.to_s.encode('UTF-8', invalid: :replace, undef: :replace, replace: '?')}
+    {tryout: self, audition_journal: self.audition_journal, url: self.resume.service_url}
   end
 end
