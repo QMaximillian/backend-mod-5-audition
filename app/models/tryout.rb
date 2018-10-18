ac1d1e97e728130cbc8acd84bdd7276df00d5802
QMaximillian <quinnlashinsky@gmail.com>
2018-10-18 00:45:46 -0400

    Commit

--- app/models/tryout.rb
+++ app/models/tryout.rb
@@ -3,4 +3,9 @@ class Tryout < ApplicationRecord
   belongs_to :audition
   has_one :audition_journal
   has_one_attached :resume
+  has_one_attached :headshot
+
+  def service_url_link
+    self.resume.service_url
+  end
 end
