570c9a50b5626eac4ce3b4690261bd0a9dc0f6d0
QMaximillian <quinnlashinsky@gmail.com>
2018-10-19 00:54:03 -0400

    Commit

--- app/serializers/actor_serializer.rb
+++ app/serializers/actor_serializer.rb
@@ -1,7 +1,7 @@
 class ActorSerializer
   include FastJsonapi::ObjectSerializer
   set_type :actor
-  attributes :first_name, :last_name, :email, :birthday, :phone_number, :equity, :gender, :city, :height, :ethnicity, :vocal_range, :audition_journals, :tryouts
+  attributes :first_name, :last_name, :email, :birthday, :phone_number, :equity, :gender, :city, :height, :ethnicity, :vocal_range, :audition_journals, :tryouts, :auditions
 
 
 end
