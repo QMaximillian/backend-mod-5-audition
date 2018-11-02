0095f22d4e44c0cdd735ffa9d41c6baf22d02fda
QMaximillian <quinnlashinsky@gmail.com>
2018-11-02 19:19:19 -0400

    Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -101,15 +101,15 @@ Show.create(season_id: 2, show_name: "Usual Girls", show_description: "How do gi
 #
 Show.create(season_id: 2, show_name: "True West", show_description: "True West is about the sibling rivalry between two estranged brothers who have reconnected. The play begins with brothers, Austin and Lee, sitting in their mother's house. This is the first time they've seen one another in five years. The two are not on good terms, but Austin attempts to appease his older brother, who is more dominant.", location: "NYC")
 
-Show.create(season_id: 2, show_name: "Merrily We Roll Along", show_description: "Franklin Shepard is a rich, famous, and influential songwriter and film producer. As the years roll back over 20 years of his life, we see how he went from penniless composer to wealthy producer, and what he gave up to get there.", location: "NYC")
-
-Show.create(season_id: 2, show_name: "Kiss Me, Kate", show_description: "Egotistical leading man, director, and producer Fred Graham is reunited with his ex-wife, Lilli Vanessi, when the two are forced to play opposite one another in a new production of Shakespeare's Taming of the Shrew. The battle of the sexes continues onstage and off, as it becomes clear that, as much as this couple profess to hate each other, they are also still in love.", location: "NYC")
-
-Show.create(season_id: 2, show_name: "All My Sons", show_description: "Joe Keller, a successful businessman, lives comfortably with his wife, Kate, and son, Chris, in a suburban American neighborhood. They have only one sadness in their lives – the loss of their other son, Larry, who went missing in World War II. After three years, Kate still clings to the hope that her son is alive.", location: "NYC")
-
-Show.create(season_id: 2, show_name: "Something Clean", show_description: "Through the eyes of a wife and mother, Something Clean examines the aftermath of a sex crime—guilt, the grief, and the ways we grapple with the unthinkable.", location: "NYC")
-
-Show.create(season_id: 2, show_name: "Toni Stone", show_description: "Toni Stone is an encyclopedia of baseball stats. She’s got a great arm. And she doesn’t understand why she can’t play with the boys.", location: "NYC")
+# Show.create(season_id: 2, show_name: "Merrily We Roll Along", show_description: "Franklin Shepard is a rich, famous, and influential songwriter and film producer. As the years roll back over 20 years of his life, we see how he went from penniless composer to wealthy producer, and what he gave up to get there.", location: "NYC")
+#
+# Show.create(season_id: 2, show_name: "Kiss Me, Kate", show_description: "Egotistical leading man, director, and producer Fred Graham is reunited with his ex-wife, Lilli Vanessi, when the two are forced to play opposite one another in a new production of Shakespeare's Taming of the Shrew. The battle of the sexes continues onstage and off, as it becomes clear that, as much as this couple profess to hate each other, they are also still in love.", location: "NYC")
+#
+# Show.create(season_id: 2, show_name: "All My Sons", show_description: "Joe Keller, a successful businessman, lives comfortably with his wife, Kate, and son, Chris, in a suburban American neighborhood. They have only one sadness in their lives – the loss of their other son, Larry, who went missing in World War II. After three years, Kate still clings to the hope that her son is alive.", location: "NYC")
+#
+# Show.create(season_id: 2, show_name: "Something Clean", show_description: "Through the eyes of a wife and mother, Something Clean examines the aftermath of a sex crime—guilt, the grief, and the ways we grapple with the unthinkable.", location: "NYC")
+#
+# Show.create(season_id: 2, show_name: "Toni Stone", show_description: "Toni Stone is an encyclopedia of baseball stats. She’s got a great arm. And she doesn’t understand why she can’t play with the boys.", location: "NYC")
 
 Show.create(season_id: 3, show_name: "Good Grief", show_description: "Nkechi was a good Nigerian-American girl. She did everything right. Went to med school. Made plans. Then life happened. And plans changed. A first-generation coming-of-age journey of love, loss, and growing into adulthood, GOOD GRIEF follows Nkechi as she navigates Pennsylvania’s suburbs alongside her childhood crush, her would-be-philosopher brother, and her immigrant parents", location: "NYC")
 
@@ -154,84 +154,76 @@ Always bring your Equity Membership card to auditions.", seeking: "Equity actors
 Female, early to mid 30’s, Latinx, East Asian, Native American, Middle Eastern or Multiracial. She is a well-respected doctor who befriends F, partly just to see if it is possible to have a true friendship with someone from the oppressing country. B is very level headed, open-minded and warm. Her generosity of spirit is put to the test when she is falsely accused of harming Q.
 WOULD ALSO PLAY Q ON EVERY OTHER PERFORMANCE.\n\n
 
-Q
+Q\n\n
 Female, mid 20’s, Latinx, East Asian, Native American, Middle Eastern or Multiracial. She has newly arrived in Country X, where her fiancé has moved to take advantage of a good business opportunity. She is excited to get to know the culture from within but when things get hard, she discovers she harbors some unconscious prejudices. She is curious, intellectual and though wide-eyed, she falls short of being naive.
 WOULD ALSO PLAY B ON EVERY OTHER PERFORMANCE.\n\n
 
-F
+F\n\n
 Female, mid 30’s, Indian or South Asian. A teacher, she has just moved to Country X for a job at the National School. She is aware of all it means to be from the oppressing country and tries to be a positive force. She is kind, open-hearted and a great conversationalist. She’s very aware of and adept at talking about the prevailing racial/political issues.
 WOULD ALSO PLAY H ON EVERY OTHER PERFORMANCE.\n\n
 
-H
+H\n\n
 Female, 30’s or early 40’s, African American. H is the most outspoken member of her social group and is a staunch defender of Country X’s rights. She is compassionately passionate and has innate leadership qualities, so people listen when she holds court. She is smart, fiery and a little unpredictable. WOULD ALSO PLAY F ON EVERY OTHER PERFORMANCE.\n\n
 
-O
+O\n\n
 Male, early to mid 30’s, African American. A businessman, O is the peacemaker of the group. He tries to see what ever issue he is looking at from all sides. He is gregarious, happy and has a joyful openness. WOULD ALSO PLAY R ON EVERY OTHER PERFORMANCE.\n\n
 
-R
+R\n\n
 Male, mid 20’s to early 30’s, Indian or South Asian. He is Q’s fiancé and he has found moving to Country X more complicated than he expected. He is glad Q has arrived but worries she doesn’t quite understand the landscape. He tries hard and can be playful. He is a real guy’s guy, a good friend and protector. WOULD ALSO PLAY O ON EVERY OTHER PERFORMANCE.\n\n
 
-J
+J\n\n
 (also to play Mosquito)
 Male, 40’s, Latinx, East Asian, Native American, Middle Eastern or Multiracial. J is a Country Y citizen who is a “Security Liaison” in Country X. He is R’s friend and confidante. He has military command but still has soft edges -the type of leader who commands respect from all kinds. WOULD ALSO PLAY D ON EVERY OTHER PERFORMANCE.\n\n
 
-D
+D\n\n
 (also to play S and Gecko)
 Male, 40’s, Latinx, East Asian, Native American or Multiracial. D is the Head Doctor at the hospital where B works. He has all the power and arrogance his position implies, but at heart he is a grounded, kind and humane person. WOULD ALSO PLAY J ON EVERY OTHER PERFORMANCE.\n\n
 
-G
+G\n\n
 any gender, late 40’s - 60’s, people of color. G is a Religious Studies Professor. G serves as the ultimate guide and conscience of the play. G is wise, calm, and spiritual. Need an actor with great facility for language, who can seduce an audience and make a real connection while delivering a direct address.", location: "
 Actors' Equity New York Audition Center\n
 165 W 46th St\n
 16th Fl\n
 New York, NY 10036")
 #
-Audition.create(show_id: 3, show_name: "Lobby Hero", audition_date: "Wed, 11 Jul 2018", audition_information: "Please bring 2 songs, and 2 monologues", cast: false, begin_audition: "2018-10-25T12:30:00Z", end_audition: "2018-10-25T13:30:00Z", time_slots: 15, state: "New York City, New York", call_type: "EPA", contract: "LOA-NYC $582/week", personnel: "Written by Christopher Chen
-Directed by Saheem Ali", show_dates: "First Rehearsal: March 25, 2019
-Tech: April 16 - 21, 2019
-First Preview: April 23, 2019
-Opens: May 4, 2019
-Closing: May 26, 2019
-Possible Extension through June 23", other: "EPA Procedures are in effect for this audition.
+Audition.create(show_id: 3, show_name: "Lobby Hero", audition_date: "Wed, 11 Jul 2018", audition_information: "Please bring 2 songs, and 2 monologues", cast: false, begin_audition: "2018-10-25T12:30:00Z", end_audition: "2018-10-25T13:30:00Z", time_slots: 15, state: "New York City, New York", call_type: "EPA", contract: "LOA-NYC $582/week", personnel: "
+Written by Christopher Chen\n
 
-An Equity Monitor will be provided.
+Directed by Saheem Ali", show_dates: "First Rehearsal: March 25, 2019\n
+Tech: April 16 - 21, 2019\n
+First Preview: April 23, 2019\n
+Opens: May 4, 2019\n
+Closing: May 26, 2019\n
+Possible Extension through June 23", other: "EPA Procedures are in effect for this audition.\n
 
-Equity’s contracts prohibit discrimination. Equity is committed to diversity and encourages all its employers to engage in a policy of equal employment opportunity designed to promote a positive model of inclusion. As such, Equity encourages performers of all ethnicities, gender identities, and ages, as well as performers with disabilities, to attend every audition.
+An Equity Monitor will be provided.\n
+
+Equity’s contracts prohibit discrimination. Equity is committed to diversity and encourages all its employers to engage in a policy of equal employment opportunity designed to promote a positive model of inclusion. As such, Equity encourages performers of all ethnicities, gender identities, and ages, as well as performers with disabilities, to attend every audition.\n
 
 Always bring your Equity Membership card to auditions.", seeking: "Equity actors for 9 roles. See breakdown.", breakdown: "B
 Female, early to mid 30’s, Latinx, East Asian, Native American, Middle Eastern or Multiracial. She is a well-respected doctor who befriends F, partly just to see if it is possible to have a true friendship with someone from the oppressing country. B is very level headed, open-minded and warm. Her generosity of spirit is put to the test when she is falsely accused of harming Q.
-WOULD ALSO PLAY Q ON EVERY OTHER PERFORMANCE.
+WOULD ALSO PLAY Q ON EVERY OTHER PERFORMANCE.\n\n
 
-Q
-Female, mid 20’s, Latinx, East Asian, Native American, Middle Eastern or Multiracial. She has newly arrived in Country X, where her fiancé has moved to take advantage of a good business opportunity. She is excited to get to know the culture from within but when things get hard, she discovers she harbors some unconscious prejudices. She is curious, intellectual and though wide-eyed, she falls short of being naive.
-WOULD ALSO PLAY B ON EVERY OTHER PERFORMANCE.
+Character 1\n\n
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vestibulum turpis nisl, a condimentum tellus lobortis nec. Aliquam finibus metus id massa pellentesque, id semper urna commodo. Nam laoreet arcu sed diam semper pellentesque. Vestibulum a sollicitudin sapien, vitae convallis nunc. Quisque semper sapien orci, id vehicula odio elementum in. Phasellus et nunc ullamcorper lorem pretium cursus in non tellus. Praesent in tortor felis. Cras dui nisl, consectetur id lectus eu, porttitor fringilla magna. Mauris id ipsum quam. Phasellus ante ex, imperdiet eu vulputate et, tempor ut augue. Proin eget viverra quam. Mauris diam mauris, congue sed ante sit amet, dignissim lacinia lorem.\n\n
 
-F
-Female, mid 30’s, Indian or South Asian. A teacher, she has just moved to Country X for a job at the National School. She is aware of all it means to be from the oppressing country and tries to be a positive force. She is kind, open-hearted and a great conversationalist. She’s very aware of and adept at talking about the prevailing racial/political issues.
-WOULD ALSO PLAY H ON EVERY OTHER PERFORMANCE.
+Character 2\n\n
+Aenean feugiat congue leo quis imperdiet. Morbi euismod placerat rhoncus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus molestie suscipit faucibus. Duis ut lacus sit amet metus fermentum pellentesque quis et urna. Ut et nibh nec eros commodo ornare at quis risus. Vestibulum eu purus sed felis sagittis vulputate non sit amet elit. Suspendisse est nulla, aliquam et metus nec, facilisis pulvinar sapien. Nunc blandit tincidunt massa ut sagittis. Vestibulum ac nunc quis risus fermentum cursus non imperdiet orci. Proin ac porttitor lacus, et feugiat urna. Maecenas bibendum nunc sit amet interdum aliquet. Proin semper augue in risus condimentum viverra. Curabitur rutrum lacus felis, at laoreet felis viverra a. Fusce fermentum arcu id sem faucibus mattis. Donec iaculis libero condimentum, suscipit est quis, ultrices lorem.\n\n
 
-H
-Female, 30’s or early 40’s, African American. H is the most outspoken member of her social group and is a staunch defender of Country X’s rights. She is compassionately passionate and has innate leadership qualities, so people listen when she holds court. She is smart, fiery and a little unpredictable. WOULD ALSO PLAY F ON EVERY OTHER PERFORMANCE.
+Character 3\n\n
+Sed eu risus at neque viverra lacinia sed quis mi. Phasellus ut tellus mauris. Proin vitae eleifend orci. Vivamus sagittis vehicula lectus eu fermentum. Suspendisse vel lorem pretium, tincidunt urna tincidunt, mollis massa. Donec lacinia felis nulla, vitae iaculis elit pretium ut. Sed ac fringilla dui. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Cras eget interdum arcu.\n\n
 
-O
-Male, early to mid 30’s, African American. A businessman, O is the peacemaker of the group. He tries to see what ever issue he is looking at from all sides. He is gregarious, happy and has a joyful openness. WOULD ALSO PLAY R ON EVERY OTHER PERFORMANCE.
+Character 4\n\n
+Curabitur nec risus et sapien aliquet rutrum. Etiam ut blandit erat, at scelerisque nulla. Quisque faucibus vestibulum justo id lacinia. Ut scelerisque nec est nec congue. Nunc consequat tincidunt dignissim. Maecenas sit amet neque in nisi volutpat semper quis non ante. Ut aliquam lorem velit, eget aliquam tellus egestas vulputate. Donec efficitur blandit dapibus. Phasellus malesuada dui sit amet nisi sodales tincidunt. Pellentesque posuere pellentesque consectetur. Etiam ac purus a tellus auctor lacinia et ut tortor. Vivamus interdum tincidunt odio, vitae aliquet lacus egestas eu.\n\n
 
-R
-Male, mid 20’s to early 30’s, Indian or South Asian. He is Q’s fiancé and he has found moving to Country X more complicated than he expected. He is glad Q has arrived but worries she doesn’t quite understand the landscape. He tries hard and can be playful. He is a real guy’s guy, a good friend and protector. WOULD ALSO PLAY O ON EVERY OTHER PERFORMANCE.
+Character 5\n\n
+Curabitur nec risus et sapien aliquet rutrum. Etiam ut blandit erat, at scelerisque nulla. Quisque faucibus vestibulum justo id lacinia. Ut scelerisque nec est nec congue. Nunc consequat tincidunt dignissim. Maecenas sit amet neque in nisi volutpat semper quis non ante. Ut aliquam lorem velit, eget aliquam tellus egestas vulputate. Donec efficitur blandit dapibus. Phasellus malesuada dui sit amet nisi sodales tincidunt. Pellentesque posuere pellentesque consectetur. Etiam ac purus a tellus auctor lacinia et ut tortor. Vivamus interdum tincidunt odio, vitae aliquet lacus egestas eu.\n\n
 
-J
-(also to play Mosquito)
-Male, 40’s, Latinx, East Asian, Native American, Middle Eastern or Multiracial. J is a Country Y citizen who is a “Security Liaison” in Country X. He is R’s friend and confidante. He has military command but still has soft edges -the type of leader who commands respect from all kinds. WOULD ALSO PLAY D ON EVERY OTHER PERFORMANCE.
-
-D
-(also to play S and Gecko)
-Male, 40’s, Latinx, East Asian, Native American or Multiracial. D is the Head Doctor at the hospital where B works. He has all the power and arrogance his position implies, but at heart he is a grounded, kind and humane person. WOULD ALSO PLAY J ON EVERY OTHER PERFORMANCE.
-
-G
-any gender, late 40’s - 60’s, people of color. G is a Religious Studies Professor. G serves as the ultimate guide and conscience of the play. G is wise, calm, and spiritual. Need an actor with great facility for language, who can seduce an audience and make a real connection while delivering a direct address.", location: "
-Actors' Equity New York Audition Center
-165 W 46th St
-16th Fl
+Charater 6\n\n
+Quisque commodo metus vel dui consectetur ornare. Vestibulum at odio eu urna sollicitudin rhoncus. Nam vehicula tellus in sagittis ornare. Donec ut enim vel lorem commodo dignissim a vel orci. Phasellus consectetur vitae ligula ac tempor. Maecenas tristique nec tellus tempor tincidunt. Cras erat quam, ullamcorper aliquet dignissim ut, placerat eu est. Nullam urna sapien, ullamcorper non rutrum id, mattis at tellus. Fusce convallis ante vitae dapibus blandit. Nulla pretium nisi turpis, quis pharetra nulla luctus ac. Suspendisse egestas placerat felis. Quisque sapien ex, venenatis sed ornare at, auctor vel sem. Pellentesque est justo, viverra quis convallis sed, molestie non ex.", location: "
+Actors' Equity New York Audition Center\n
+165 W 46th St\n
+16th Fl\n
 New York, NY 10036")
 
 # Audition.create(show_id: 4, show_name: "Mary Page Marlowe", audition_information: "Please bring 2 songs, and 2 monologues", location: "Ripley-Grier Studios (520), 520 8th Ave, 17th Floor", cast: false, begin_audition: "Sun, 8 Jul 2018 08:00:00 -0000", end_audition: "Sun, 8 Jul 2018 12:00:00 -0000", time_slots: 30, state: "New York City, New York")
